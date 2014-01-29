b2df0fd4192e7baf9b603d544e98690bd3e16bb7
greg5green <greggreen@gmail.com>
2014-01-29 02:31:04 -0500

    Added initial handling of user input (through creating DOM nodes

--- dist/assets/js/jstally.js
+++ dist/assets/js/jstally.js
@@ -1,7 +1,38 @@
-;(function($) {
-    function tally(param) {
-        return param;
+;var jsTally = (function($) {
+
+    function splitUserInput(input) {
+        return input.split('\n');
     }
 
-    window.jstally = tally;
+    function sortUserInput(input) {
+        return input.sort(function(a, b) {
+            return a > b;
+        });
+    }
+
+    return {
+        init: function() {
+            $('#tally-input-form').on('submit', function(event) {
+                event.preventDefault();
+
+                handlerUserInput(event);
+            });
+        },
+
+        handleUserInput: function(event) {
+            var userInput = splitUserInput(event.target.elements[0].value),
+                sortedUserInput = sortUserInput(userInput);
+
+            this.createDomNodesFromUserInput(sortedUserInput);
+        },
+
+        createDomNodesFromUserInput: function (input) {
+
+        }
+    }
+
 })(jQuery);
+
+jQuery(function() {
+    jsTally.init();
+});
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -3,17 +3,35 @@
 (function() {
     'use strict';
 
-    describe('tally', function() {
-        describe('the only/future init method', function() {
-            it('should return the same thing you pass it', function() {
-                var one = true,
-                    two = [1, 2, 3],
-                    three = { myKey: 'value' };
+    describe('Handling initial user input before DOM node creation', function() {
+        var inputArg;
 
-                assert(one, jstally(one));
-                assert(two, jstally(two));
-                assert(three, jstally(three));
+        before(function() {
+            sinon.spy(jsTally, 'createDomNodesFromUserInput');
+        });
+
+        beforeEach(function() {
+            jsTally.handleUserInput({
+                target: {
+                    elements: [{
+                        value: '10\n1\nthree\nfour'
+                    }]
+                }
             });
+
+            inputArg = jsTally.createDomNodesFromUserInput.args[0][0];
+        });
+
+        afterEach(function() {
+            jsTally.createDomNodesFromUserInput.reset();
+        });
+
+        it('should be split on new lines', function() {
+            inputArg.should.have.length(4);
+        });
+        it('should be alphabatized', function() {
+            inputArg.should.be.eql(['1', '10', 'four', 'three']);
         });
     });
+
 })();
